8624d914e91ce624f71d0d57df1199d05a957aa9
Stefano Norcia <stefano.norcia@gmail.com>
2022-03-24 12:29:05 +0100

    removed comments

--- client.py
+++ client.py
@@ -5,10 +5,6 @@ import types
 HOST = "127.0.0.1"
 PORT = 65431
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b"Hello")
-
 sel = selectors.DefaultSelector()
 messages = [b"Hello 1", b"Hello 2"]
 
