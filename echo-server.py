79efb3e069759dbb01e67e923c11014d29b69322
Stefano Norcia <stefano.norcia@gmail.com>
2022-03-24 12:23:06 +0100

    server

---
+++ echo-server.py
@@ -0,0 +1,66 @@
+import selectors
+import socket
+import types
+
+HOST = "127.0.0.1"
+PORT = 65431
+
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind((HOST, PORT))
+#     s.listen()
+#     conn, addr = s.accept()
+#     with conn:
+#         print(addr)
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.sendall(data)
+
+def accept(sock):
+    conn, addr = sock.accept()
+    print("accepting connection")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def connect(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            print(recv_data)
+            data.outb += recv_data
+        else:
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+
+sel = selectors.DefaultSelector()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept(key.fileobj)  # accettazione connessione
+            else:
+                connect(key, mask)  # lavoro sul socket
+except KeyboardInterrupt:
+    print("Key pressed")
+finally:
+    sel.close()
